7e67a86f2f585fd2c64b067d1648a709fae7011d
Adel Mouhaidly <adel.mouhaidly6@gmail.com>
2025-11-08 17:18:08 -0300

    Arrumand oerros

--- Challenge-IOT/start.sh
+++ Challenge-IOT/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gunicorn backend:app --bind 0.0.0.0:${PORT:-8000} --workers 2 --timeout 120 --worker-class uvicorn.workers.UvicornWorker
+gunicorn backend:app --bind 0.0.0.0:${PORT:-8000} --workers 1 --timeout 600 --worker-class uvicorn.workers.UvicornWorker --max-requests 1000 --max-requests-jitter 100
 
